1b3ef556b8765ac54973a55f914e1eacc75d914c
vijay-2210 <73438496+vijay-2210@users.noreply.github.com>
2024-08-23 10:47:56 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -7,25 +7,44 @@ int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    // Stub always returns 500 for this test case
+    return 500;
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        // Increment the failure count if the return code is not 200
+        alertFailureCount += 1;
     }
 }
 
-int main() {
+float actualCelicusReceived;
+int networkAlertCallCount = 0;
+
+int networkAlertMock(float celcius) {
+    actualCelicusReceived = celcius;
+    ++networkAlertCallCount;
+    return 500;
+}
+
+void stateBasedTest() {
     alertInCelcius(400.5);
+    assert(alertFailureCount == 1);
+}
+
+void behaviorTest() {
     alertInCelcius(303.6);
+    assert(alertFailureCount == 2);  // Since stateBasedTest already increments by 1
+    float expectedCelToBeReceivedByDependency = (303.6 - 32) * 5 / 9;
+    assert(actualCelicusReceived == expectedCelToBeReceivedByDependency);
+    assert(networkAlertCallCount == 1);
+}
+
+int main() {
+    stateBasedTest();
+    behaviorTest();
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
