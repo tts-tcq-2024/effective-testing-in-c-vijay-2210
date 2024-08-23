405c36be69be84cf54333d35a84de120c032c9d9
vijay-2210 <73438496+vijay-2210@users.noreply.github.com>
2024-08-23 10:48:36 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+// Buffer to capture the output
+#define BUFFER_SIZE 4096
+char capturedOutput[BUFFER_SIZE];
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -7,14 +12,30 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
         }
     }
     return i * j;
 }
 
 int main() {
+    char buffer[1000] = {0};
     int result = printColorMap();
+    
+    const char* expectedMajorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* expectedMinorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+    int position = 0;
+    
+    for (int majorIndex = 0; majorIndex < 5; majorIndex++) {
+        for (int minorIndex = 0; minorIndex < 5; minorIndex++) {
+            char expectedLine[50];
+            snprintf(expectedLine, sizeof(expectedLine), "%d | %s | %s\n", position, expectedMajorColors[majorIndex], expectedMinorColors[minorIndex]);
+            assert(strncmp(buffer + position * strlen(expectedLine), expectedLine, strlen(expectedLine)) == 0);
+            
+            position++;
+        }
+    }
     assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
